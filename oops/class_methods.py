a14d82f01fe162bef5ef80529d386bdf210cdd6a
shannu1653 <pentashanmukha2002@gmail.com>
2025-07-27 12:25:21 +0530

    methods

--- oops/class_methods.py
+++ oops/class_methods.py
@@ -3,8 +3,15 @@ class Instructor:
     def __init__(self,name,address):
         self.name=name
         self.address=address
-    def display(self):
-        print(f"hi {self.name} , youre from {self.address}")
+    def display(self,subject_name):
+        print(f"hi {self.name} , youre from {self.address} a in am teaching {subject_name}")
+    def updated_followers(self,follower_name):
+        self.followers +=1
 instructor_1=Instructor('shannu','nemalam')
 print(instructor_1.name,instructor_1.followers)
-instructor_1.display()
+instructor_1.display('python')
+instructor_1.updated_followers('hello')
+print(instructor_1.followers)
+instructor_2=Instructor('shannu','ram')
+instructor_2.updated_followers('rama')
+print(instructor_2.followers)

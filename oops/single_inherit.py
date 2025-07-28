4d15097cd1c6f6bb7d2cbd3ca83fb0923cad5323
shannu1653 <pentashanmukha2002@gmail.com>
2025-07-28 15:30:28 +0530

    single

--- oops/single_inherit.py
+++ oops/single_inherit.py
@@ -8,15 +8,48 @@
 4.hierarical   "
 5.hybrid'''
 
-#single inheritance
+# #single inheritance
 
-class Parent:
-    def Pmethod(self):
-        print('i am a method from parent')
+# class Parent:
+#     def Pmethod(self):
+#         print('i am a method from parent')
+#     def Pmethod2(self):
+#         print("i am a second method from parent")
+    
 
-class Child(Parent):
-    def Cmethod(self):
-        print('i am a method from child')
-obj1=Child()
-obj1.Pmethod()
-obj1.Cmethod()
+# class Child(Parent):
+#     def Cmethod(a):
+#         print('i am a method from child')
+#         super().Pmethod2() #calling method from super class using super()
+# obj1=Child()
+# obj1.Pmethod()
+# obj1.Cmethod()
+
+
+class User:
+    def __init__(self,name,email):
+        self.name=name
+        self.email=email
+class Student(User):
+    def __init__(self, name, email,enrolledcourses):
+        super().__init__(name, email)
+        self.enrolledcourses=enrolledcourses
+    def getCourses(self):
+        print(f"{self.name} is learning {self.enrolledcourses}")
+    def removeCourses(self,coursename):
+        self.enrolledcourses.remove(coursename)
+        self.getCourses()
+    def addCourses(self,course):
+        self.enrolledcourses.append(course)
+        self.getCourses()
+
+# class Trainer(User):
+#     def __init__(self,name,email,courses_training):
+student_object=Student("shanmukha","shanmukha@gmail.com",['html','python','js'])
+student_object.getCourses()
+student_object.removeCourses('html')
+student_object.addCourses('sql')
+
+
+         
+

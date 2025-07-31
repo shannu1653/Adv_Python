c9d909966b1ac9bb00a757de6f69838fffe166d0
shannu1653 <pentashanmukha2002@gmail.com>
2025-07-31 23:36:47 +0530

    method

--- oops/method_overiding.py
+++ oops/method_overiding.py
@@ -16,3 +16,42 @@ cycle=Cycle()
 car.speed()
 cycle.speed()
 
+
+#method overiding
+# Parent class
+class Animal:
+    def speak(self):
+        print("Animal makes a sound")
+
+# Child class that overrides speak()
+class Dog(Animal):
+    def speak(self):
+        print("Dog barks")
+
+# Creating objects
+a = Animal()
+d = Dog()
+
+# Calling methods
+a.speak()  # Output: Animal makes a sound
+d.speak()  # Output: Dog barks
+
+
+class Bird:
+    def fly(self):
+        print("Bird can fly")
+
+class Parrot(Bird):
+    def fly(self):
+        print("Parrot can fly faster")
+        super().fly()  # Call the parent class method
+
+p = Parrot()
+p.fly()
+
+# Output:
+# Parrot can fly faster
+# Bird can fly
+
+
+

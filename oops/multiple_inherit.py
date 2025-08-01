9f457960e24123ec5a17b42ed329df81cf730ed3
shannu1653 <pentashanmukha2002@gmail.com>
2025-08-01 11:52:26 +0530

    examples

--- oops/multiple_inherit.py
+++ oops/multiple_inherit.py
@@ -13,16 +13,42 @@
 # obj1=Child()
 # obj1.Cmethod()
 
-class A:
-    def display(self):
-        print("A class")
-class B:
-    def display(self):
-        print("B class")        
-class C(A,B):
-    def display(self):
-        super().display()
-        print("C class")
-        B().display()
-obj=C()
-obj.display()
+# class A:
+#     def display(self):
+#         print("A class")
+# class B:
+#     def display(self):
+#         print("B class")        
+# class C(A,B):
+#     def display(self):
+#         super().display()
+#         print("C class")
+#         B().display()
+# obj=C()
+# obj.display()
+
+class Human:
+    def eat(self):
+        print("I can eat")
+    def work(self):
+        print("I can work")
+
+class Male:
+    def flirt(self):
+        print("I can flirt")
+    def work(self):
+        print("I can code")
+
+
+class Boy(Human,Male):  #(Male,Human)approach 1
+    def sleep(self):
+        print("I can sleep")
+    def work(self):
+        print("I can test")
+boy_1=Boy()
+# boy_1.flirt()
+# boy_1.eat()
+boy_1.work()
+# Male.work(boy_1)#approach 2
+# Human.work(boy_1)
+print(Boy.mro())

f6c130d6c852010f2b4b7b2439c5fa56b4fdfae7
shannu1653 <pentashanmukha2002@gmail.com>
2025-08-01 19:39:30 +0530

    multiple_inherotance

--- oops/multiple_inherit.py
+++ oops/multiple_inherit.py
@@ -1,31 +1,31 @@
-# #multiple inheritance
-# class Parent1:
-#     def P1method(self):
-#         print("i am a method from parent1")
-# class Parent2:
-#     def P2method(self):
-#         print("i am a method from parent2")
-# class Child(Parent2,Parent1):
-#     def Cmethod(self):
-#         super().P1method()
-#         super().P2method()
-#         print("i am method from child")
-# obj1=Child()
-# obj1.Cmethod()
+#multiple inheritance
+class Parent1:
+    def P1method(self):
+        print("i am a method from parent1")
+class Parent2:
+    def P2method(self):
+        print("i am a method from parent2")
+class Child(Parent2,Parent1):
+    def Cmethod(self):
+        super().P1method()
+        super().P2method()
+        print("i am method from child")
+obj1=Child()
+obj1.Cmethod()
 
-# class A:
-#     def display(self):
-#         print("A class")
-# class B:
-#     def display(self):
-#         print("B class")        
-# class C(A,B):
-#     def display(self):
-#         super().display()
-#         print("C class")
-#         B().display()
-# obj=C()
-# obj.display()
+class A:
+    def display(self):
+        print("A class")
+class B:
+    def display(self):
+        print("B class")        
+class C(A,B):
+    def display(self):
+        super().display()
+        print("C class")
+        B().display()
+obj=C()
+obj.display()
 
 class Human:
     def eat(self):
@@ -49,6 +49,47 @@ boy_1=Boy()
 # boy_1.flirt()
 # boy_1.eat()
 boy_1.work()
-# Male.work(boy_1)#approach 2
-# Human.work(boy_1)
+Male.work(boy_1)#approach 2
+Human.work(boy_1)
 print(Boy.mro())
+
+
+class Human:
+    def __init__(self,num_heart):
+        print("cslling init from Human")
+        self.eys=2
+        self.nose=2
+        self.num_heart=num_heart
+    def eat(self):
+        print("I can eat")
+    def work(self):
+        print("I can work")
+
+class Male:
+    def __init__(self,name):
+        print("calling init from Male")
+        self.name=name
+    def flirt(self):
+        print("I can flirt")
+    def work(self):
+        print("I can code")
+
+
+class Boy(Human,Male):  #(Male,Human)approach 1
+    def __init__(self,name,languge,num_heart):
+        Human.__init__(self,num_heart)
+        Male.__init__(self,name)
+        self.language=languge        
+    def sleep(self):
+        print("I can sleep")
+    def work(self):
+        print("I can test")
+    def display(self):
+        print(f"Hi i am {self.name} i am learning {self.language} and i have {self.num_heart} hearts")
+boy_1=Boy("shannu","pyhton",3)
+# boy_1.flirt()
+# boy_1.eat()
+print(boy_1.nose)
+print(boy_1.num_heart)
+print(boy_1.language)
+boy_1.display()

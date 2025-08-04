71c9c1ae849d9613a02446520fbd73d15173165e
shannu1653 <pentashanmukha2002@gmail.com>
2025-08-04 15:18:06 +0530

    polymorphism

--- oops/polymorphism.py
+++ oops/polymorphism.py
@@ -1,3 +1,5 @@
+#Polymorphsim:Functions with same name bahaving differently in different classes
+
 class Dog:
     def speak(self):
         return 'woof!!'
@@ -14,3 +16,23 @@ cat=Cat()
 tiger=Tiger()
 animated_sound(dog)
 animated_sound(cat)
+
+
+#ex2
+class SendMsg():
+    def send(self, message):
+        print(f"sms sent : {message}")
+class SendEmail():
+    def send(self, message):
+        print(f"Email sent: {message}")
+class Notifier():
+    def send(self, message):
+        print(f"Notifier send : {message}")
+
+def sendMsg(sender, message):
+    sender.send(message)
+
+sendMsg(SendMsg(),'Hi your OTP is 2343')
+sendMsg(SendEmail(),'congratulation your are hire')
+sendMsg(Notifier(),'succefully created 10xxxxx your bankAccount')
+

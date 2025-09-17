6d4154fec419953333aff2e8fae13d0da4169bc1
shannu1653 <pentashanmukha2002@gmail.com>
2025-09-17 16:59:59 +0530

    python

---
+++ exception/try_exception.py
@@ -0,0 +1,9 @@
+#ModulNOtFounEroor
+try:
+    import srebd
+except ModuleNotFoundError as a:
+    print("module not found :",a)
+
+#we use only run time errors only in python
+#avoid stuck in our programm,avoid crash our websites
+#use in real time projects in our code
